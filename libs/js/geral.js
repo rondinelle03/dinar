dc80b5330143c8c28b619eb505a69b9d941e3184
rondinelle03 <rondinellefreire@gmail.com>
2020-09-04 10:02:59 -0300

    Montagem de telas para Area Restrita do TCE

--- libs/js/geral.js
+++ libs/js/geral.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
 
+	$('.list-info').owlCarousel({
+	    loop:true,
+	    margin:12,
+	    nav:false,
+	    responsive:{
+	        0:{
+	            items:3
+	        },
+	        600:{
+	            items:4
+	        },
+	        1000:{
+	            items:12
+	        }
+	    }
+	})
 
 });
